4bb1cbf3640b1d68e777683fd341e1575edcc5fa
John Kim <kakwak123@gmail.com>
2021-09-09 16:47:24 +1000

    page example added

--- src/RootSPA.js
+++ src/RootSPA.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+import Main from "./client/pages/Main/Main";
+
 export function RootSPA() {
   return (
     <Router>
       <Switch>
-        <Route path="/customer" exact></Route>
+        <Route path="/" exact>
+          <Main />
+        </Route>
       </Switch>
     </Router>
   );
---
+++ src/client/pages/Main/Main.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function Rate(props) {
+  return (
+    <div>
+      <h1>hi</h1>
+    </div>
+  );
+}
+export default Rate;
