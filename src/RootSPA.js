55835bd28f6cd64e1cea402b31caa95eefe117e1
soosoo3 <sooyoun1999@gmail.com>
2021-09-19 00:42:43 +0900

    Login page

--- src/RootSPA.js
+++ src/RootSPA.js
@@ -2,13 +2,16 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Main from "./client/pages/Main/Main";
-
+import Login from "./client/pages/Login/Login";
 export function RootSPA() {
   return (
     <Router>
       <Switch>
         <Route path="/" exact>
           <Main />
+        </Route>
+        <Route path="/Login" exact>
+          <Login />
         </Route>
       </Switch>
     </Router>
---
+++ src/client/pages/Login/Login.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import "./Styles.css";
+import CustomInput from "./Components/UserInput";
+import Button from "./Components/Button";
+
+export default class App extends Component {
+    state = {
+        email: "",
+        password: ""
+    };
+
+    handleChange = e => {
+        this.setState({ [e.currentTarget.id]: e.currentTarget.value });
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <form className="form">
+                    <CustomInput
+                        labelText="Email"
+                        id="email"
+                        formControlProps={{
+                            fullWidth: true
+                        }}
+                        handleChange={this.handleChange}
+                        type="text"
+                    />
+                    <CustomInput
+                        labelText="Password"
+                        id="password"
+                        formControlProps={{
+                            fullWidth: true
+                        }}
+                        handleChange={this.handleChange}
+                        type="password"
+                    />
+
+                    <Button type="button" color="primary" className="form__custom-button">
+                        Log in
+          </Button>
+                </form>
+            </div>
+        );
+    }
+}
