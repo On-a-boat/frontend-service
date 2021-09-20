1b48f78e6df21491ba9f307eb084effeaf62d5b5
soosoo3 <sooyoun1999@gmail.com>
2021-09-20 11:19:09 +0900

    testing navbar

--- src/client/pages/Main/Main.js
+++ src/client/pages/Main/Main.js
@@ -1,11 +1,8 @@
 import React from "react";
 import "./Styles.css";
-import navBar from "../../shared/NavigationBar";
-export function Rate(props) {
-  return (
-    <div>
-      <h1>hi world asASDFADSFdf</h1>
-    </div>
+import NavBar from "../../shared/NavigationBar"
+
+export default function mama(props) {
+  return (<div></div>
   );
 }
-export default Rate;
