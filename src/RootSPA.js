74176ccd9ea7ab1dde401a5e70e0733f76a9d7df
soosoo3 <sooyoun1999@gmail.com>
2021-09-20 15:19:25 +0900

    response nav bar (with header)

--- src/RootSPA.js
+++ src/RootSPA.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import NavBar from "./client/shared/NavigationBar"
+import NavBar from "./client/shared/NavigationBar/NavigationBar"
 import Main from "./client/pages/Main/Main";
 import Login from "./client/pages/Login/Login";
 export function RootSPA() {
@@ -8,6 +8,7 @@ export function RootSPA() {
     <Router>
       <Switch>
         <Route path="/" exact>
+
           <NavBar />
           <Main />
         </Route>
--- src/client/pages/Main/Main.js
+++ src/client/pages/Main/Main.js
@@ -1,8 +1,7 @@
 import React from "react";
 import "./Styles.css";
-import NavBar from "../../shared/NavigationBar"
 
 export default function mama(props) {
-  return (<div></div>
+  return (<h1>dasidjaoaijaodij</h1>
   );
 }
