99215967e349899018fb21a4725164a532ed2416
John Kim <kakwak123@gmail.com>
2021-10-03 19:04:02 +1100

    Update Routes.js

--- src/Routes.js
+++ src/Routes.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
 // Components
-import Settings from './client/content/settings/Settings';
-import CRM from './client/content/crm/CRM';
-import Groups from './client/content/groups/Groups';
-import Statistics from './client/content/statistics/Statistics';
-import Email from './client/content/email/Email';
+import Settings from "./client/content/settings/Settings";
+import CRM from "./client/content/CRM/CRM";
+import Groups from "./client/content/groups/Groups";
+import Statistics from "./client/content/statistics/Statistics";
+import Email from "./client/content/email/Email";
 const RootSPA = () => {
-    return (
-        <Switch>
-            <Route exact path='/' component={CRM} />
-            <Route exact path='/groups' component={Groups} />
-            <Route exact path='/email' component={Email} />
-            <Route exact path='/statistics' component={Statistics} />
-            <Route exact path='/settings' component={Settings} />
+  return (
+    <Switch>
+      <Route exact path="/" component={CRM} />
+      <Route exact path="/groups" component={Groups} />
+      <Route exact path="/email" component={Email} />
+      <Route exact path="/statistics" component={Statistics} />
+      <Route exact path="/settings" component={Settings} />
+    </Switch>
+  );
+};
 
-
-        </Switch>
-    )
-}
-
-export default RootSPA
+export default RootSPA;
