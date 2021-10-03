3736613826f9b8c1a65057c9b85daa141f39057e
John Kim <kakwak123@gmail.com>
2021-10-03 17:12:30 +1100

    refactor routes

--- src/App.js
+++ src/App.js
@@ -1,33 +1,14 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React from "react";
 
-import * as s from './App.styles'; // bring in all the components from app.styles.js 
-import Sidebar from './client/sidebar/Sidebar';
-import Main from './client/content/Main';
-
+import * as s from "./App.styles"; // bring in all the components from app.styles.js
+import Routes from "./Routes";
 
 function App() {
-
-  const bgImage = 'images/mountain.jpg'
-  // const menuItems = ['CRM', 'Groups', 'Statistics', 'Settings'];
-  const menuItems = [
-    { name: 'CRM', to: '/', icon: '' },
-    { name: 'Groups', to: '/groups', icon: 'icons/groups.svg' },
-    { name: 'Statistics', to: '/statistics', icon: '' },
-    { name: 'Settings', to: '/settings', icon: 'icons/settings.svg' }];
-
   return (
     <s.App>
-      <Sidebar
-        ddd={bgImage}
-        menuItems={menuItems} />
-      <Main />
-
+      <Routes />
     </s.App>
-
   );
-
 }
-
 
 export default App;
--- src/client/sidebar/Sidebar.js
+++ src/client/sidebar/Sidebar.js
@@ -1,47 +1,41 @@
-import React, { useState } from 'react';
-import * as s from './Sidebar.styles'; // bring in all the components from app.styles.js 
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import * as s from "./Sidebar.styles"; // bring in all the components from app.styles.js
+import { Link } from "react-router-dom";
 
+const Sidebar = (props) => {
+  // always initialise
+  const { ddd = "", menuItems = [] } = props;
 
-const Sidebar = props => {
+  const [selected, setSelectedMenuItem] = useState(menuItems[0].name);
 
+  const handleMenuItemClick = (name) => {
+    setSelectedMenuItem(name);
+  };
 
-    // always initialise 
-    const { ddd = '', menuItems = [] } = props
-
-    const [selected, setSelectedMenuItem] = useState(menuItems[0].name);
-
-    const handleMenuItemClick = name => {
-        setSelectedMenuItem(name);
-    }
-
-    const header = "On a Boat";
-    const MenuItemsJSX = menuItems.map((item, index) => {
-        const isItemSelected = selected === item.name;
-
-        return (
-            <Link to={item.to} style={{ textDecoration: 'none' }}>
-                <s.MenuItem
-                    key={index}
-                    selected={isItemSelected}
-                    onClick={() => handleMenuItemClick(item.name)}
-                >
-                    <s.Icon src={item.icon} />
-                    <s.Text>{item.name}</s.Text>
-                </s.MenuItem></Link>)
-    })
-
-
+  const header = "On a Boat";
+  const MenuItemsJSX = menuItems.map((item, index) => {
+    const isItemSelected = selected === item.name;
 
     return (
-        <s.SideBarContainer backgroundImage={ddd}>
-            <s.SidebarHeader>{header}</s.SidebarHeader>
-            <s.MenuItemContainer>{MenuItemsJSX}</s.MenuItemContainer>
-        </s.SideBarContainer>
+      <Link to={item.to} style={{ textDecoration: "none" }}>
+        <s.MenuItem
+          key={index}
+          selected={isItemSelected}
+          onClick={() => handleMenuItemClick(item.name)}
+        >
+          <s.Icon src={item.icon} />
+          <s.Text>{item.name}</s.Text>
+        </s.MenuItem>
+      </Link>
     );
-}
+  });
+
+  return (
+    <s.SideBarContainer backgroundImage={ddd}>
+      <s.SidebarHeader>{header}</s.SidebarHeader>
+      <s.MenuItemContainer>{MenuItemsJSX}</s.MenuItemContainer>
+    </s.SideBarContainer>
+  );
+};
 
 export default Sidebar;
-
-
-
