a1ac197f69b51d48a7d0c86ab7812a072f6eb17d
Yifeng928 <88825161+Yifeng928@users.noreply.github.com>
2021-10-05 02:50:35 +1100

    Try with axios

--- src/client/content/CRM/CRM.js
+++ src/client/content/CRM/CRM.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React ,{ Component } from "react";
+import axios from 'axios';
 import * as s from './CRM.styles'; // bring in all the components from app.styles.js 
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      UserId: "",
+      FirstName: "",
+      LastName: "",
+      Age: "",
+      Gender: "",
+      Keywords: [],
+    };
+  }
 
-const CRM = () => {
+  //GET Request
+  handleOnSearch(){
+    axios.get(`http://localhost:5000/filter`,{
+      params: {
+        id: this.state.id
+      }
+    })
+    .then(res => {
+      console.log(this.state.persons);
+      this.setState({ persons: res.data });
+    });
+  }
 
-    return (<h1>CRM</h1>);
+  render(){
+	  return (<h1>test1 </h1>);
+  }
 }
 
-export default CRM;
+export default App;
