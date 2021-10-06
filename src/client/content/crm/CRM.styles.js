5d4a7adfb10cce0dccf1ffc62e69e9aba3c27a4f
John Kim <kakwak123@gmail.com>
2021-10-06 21:47:20 +1100

    fix crm

---
+++ src/client/content/crm/CRM.styles.js
@@ -0,0 +1,42 @@
+import styled from "@emotion/styled";
+
+export const CRMContainer = styled.div`
+  width: 80%;
+`;
+
+export const CreateGroupButton = styled.button`
+  height: 5%;
+`;
+
+export const TableStyles = styled.div`
+  padding: 1rem;
+
+  table {
+    border-spacing: 0;
+    border: 1px solid black;
+
+    tr {
+      :last-child {
+        td {
+          border-bottom: 0;
+        }
+      }
+    }
+
+    th,
+    td {
+      margin: 0;
+      padding: 0.5rem;
+      border-bottom: 1px solid black;
+      border-right: 1px solid black;
+
+      :last-child {
+        border-right: 0;
+      }
+    }
+  }
+
+  .pagination {
+    padding: 0.5rem;
+  }
+`;
