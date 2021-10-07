7c7d2202542bf32a08dd177e35ef97c4912e6624
Jun Hee Han <hansjun99@gmail.com>
2021-10-07 16:44:11 +1100

    Basic table display

--- src/client/content/groups/Groups.js
+++ src/client/content/groups/Groups.js
@@ -1,57 +1,229 @@
-import React from 'react';
+import React from "react";
+import { useState, useEffect, Fragment } from "react";
+import axios from 'axios';
+import {
+    useTable,
+    usePagination,
+    useRowSelect,
+    useSortBy,
+    useFilters,
+    useGlobalFilter,
+    useAsyncDebounce,
+} from "react-table";
+import DefaultColumnFilter from "../crm/filters/DefaultColumnFilter";
+import NumberRangeColumnFilter from "../crm/filters/NumberRangeColumnFilter";
+
 import * as s from '../crm/CRM.styles';
 
 
+// Copied from CRM.js
+//------------------------------------------------------------------------------------------------------------
+const IndeterminateCheckbox = React.forwardRef(
+    ({ indeterminate, ...rest }, ref) => {
+        const defaultRef = React.useRef();
+        const resolvedRef = ref || defaultRef;
 
-import namor from 'namor'
+        React.useEffect(() => {
+            resolvedRef.current.indeterminate = indeterminate;
+        }, [resolvedRef, indeterminate]);
 
-const range = len => {
-    const arr = []
-    for (let i = 0; i < len; i++) {
-        arr.push(i)
+        return (
+            <>
+                <input type="checkbox" ref={resolvedRef} {...rest} />
+            </>
+        );
     }
-    return arr
-}
+);
 
-const newGroup = () => {
-    const statusChance = Math.random()
-    return {
-        num:1, 
-        groupName: namor.generate({ words: 1, numbers: 0 }),
-        users: Math.floor(Math.random() * 100),
-        dataCreated: Math.floor(Math.random() * 100),
-        emailSent: Math.floor(Math.random() * 100),
+const Table = function ({ columns, data }) {
+    const filterTypes = React.useMemo(
+        () => ({
+            // override the default text filter to use"
+            text: (rows, id, filterValue) => {
+                return rows.filter((row) => {
+                    const rowValue = row.values[id];
+                    return rowValue !== undefined
+                        ? String(rowValue)
+                            .toLowerCase()
+                            .startsWith(String(filterValue).toLowerCase())
+                        : true;
+                });
+            },
+        }),
+        []
+    );
+    const defaultColumn = React.useMemo(
+        () => ({
+            // Let's set up our default Filter UI
+            Filter: DefaultColumnFilter,
+        }),
+        []
+    );
 
-    }
-}
+    // Use the state and functions returned from useTable to build your UI
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        prepareRow,
+        page,
+        rows,
+        state,
+        visibleColumns,
+        preGlobalFilteredRows,
+        setGlobalFilter,
+        canPreviousPage,
+        canNextPage,
+        pageOptions,
+        pageCount,
+        gotoPage,
+        nextPage,
+        previousPage,
+        selectedFlatRows,
+        state: { pageIndex, pageSize, selectedRowIds },
+    } = useTable(
+        {
+            columns,
+            data,
+            defaultColumn,
+            filterTypes,
+        },
+        useFilters,
+        useSortBy,
+        usePagination,
+        useRowSelect,
 
-function makeData(...lens) {
-    const makeDataLevel = (depth = 0) => {
-        const len = lens[depth]
-        return range(len).map(d => {
-            return {
-                ...newGroup(),
-                subRows: lens[depth + 1] ? makeDataLevel(depth + 1) : undefined,
-            }
-        })
-    }
+        (hooks) => {
+            hooks.visibleColumns.push((columns) => [
+                // Let's make a column for selection
+                {
+                    id: "selection",
+                    // The header can use the table's getToggleAllRowsSelectedProps method
+                    // to render a checkbox
+                    Header: ({ getToggleAllRowsSelectedProps }) => (
+                        <div>
+                            <IndeterminateCheckbox {...getToggleAllRowsSelectedProps()} />
+                        </div>
+                    ),
+                    // The cell can use the individual row's getToggleRowSelectedProps method
+                    // to the render a checkbox
+                    Cell: ({ row }) => (
+                        <div>
+                            <IndeterminateCheckbox {...row.getToggleRowSelectedProps()} />
+                        </div>
+                    ),
+                },
+                ...columns,
+            ]);
+        }
+    );
 
-    return makeDataLevel()
-}
+    return (
+        <>
+            <table {...getTableProps()}>
+                <thead>
+                    {headerGroups.map((headerGroup) => (
+                        <Fragment>
+                            <tr>
+                                {headerGroup.headers.map((column) => (
+                                    <th>
+                                        {column.render("Header")}
+                                        {/* Render the columns filter UI */}
+                                        <div>
+                                            {column.canFilter ? column.render("Filter") : null}
+                                        </div>
+                                    </th>
+                                ))}
+                            </tr>
+                            <tr {...headerGroup.getHeaderGroupProps()}>
+                                {headerGroup.headers.map((column) => (
+                                    // Add the sorting props to control sorting. For this example
+                                    // we can add them into the header props
+                                    <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                                        <span>
+                                            {/* no sorting for selection row */}
+                                            {/* I know it's a hardcoding, leave it for now  */}
+                                            {column.Header.length > 1
+                                                ? column.isSorted
+                                                    ? column.isSortedDesc
+                                                        ? " 🔽"
+                                                        : " 🔼"
+                                                    : "{click to sort (temp)"
+                                                : null}
+                                        </span>
+                                    </th>
+                                ))}
+                            </tr>
+                        </Fragment>
+                    ))}
+                </thead>
+
+                <tbody {...getTableBodyProps()}>
+                    {page.map((row, i) => {
+                        prepareRow(row);
+                        return (
+                            <tr {...row.getRowProps()}>
+                                {row.cells.map((cell) => {
+                                    return (
+                                        <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                                    );
+                                })}
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+
+            <div className="pagination">
+                <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+                    {"<<"}
+                </button>{" "}
+                <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+                    {"<"}
+                </button>{" "}
+                <button onClick={() => nextPage()} disabled={!canNextPage}>
+                    {">"}
+                </button>{" "}
+                <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+                    {">>"}
+                </button>{" "}
+                <span>
+                    Page{" "}
+                    <strong>
+                        {pageIndex + 1} of {pageOptions.length}
+                    </strong>{" "}
+                </span>
+            </div>
+
+            <pre>
+                <code>
+                    {
+                        localStorage.setItem(
+                            "selectedRows",
+                            JSON.stringify(selectedFlatRows.map(d => d.original))
+                        )
+                    }
+                </code>
+            </pre>
+        </>
+    );
+};
+//------------------------------------------------------------------------------------------------------------
 
 
 const Groups = () => {
+    const [data, setData] = useState([]);
 
     const columns = React.useMemo(
         () => [
 
             {
                 Header: 'No.',
-                accessor: 'num',
+                accessor: 'GroupId',
             },
             {
                 Header: 'Group Name',
-                accessor: 'groupName',
+                accessor: 'GroupName',
             },
             {
                 Header: 'Users',
@@ -59,21 +231,54 @@ const Groups = () => {
             },
             {
                 Header: 'Date Created',
-                accessor: 'dateCreated',
+                accessor: 'Date',
             },
             {
-                Header: 'Emails sent',
-                accessor: 'emailSent',
+                Header: 'Keywords',
+                accessor: 'GroupDescription',
             },
         ]
     )
 
-    const data = React.useMemo(() => makeData(100), []) // Fetch groups data from DB using axios later.
+     // Fetch groups from the Database.
+    useEffect(() => {
+        let isMounted = true;
+        const getGroup = async () => {
+            try {
+                const groups = await axios.get('http://13.54.19.72:5000/group');
 
-
+                if (isMounted) {
+                    setData(groups.data);
+                }
+                                
+                console.log(groups);
     
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        getGroup();
+        return () => {
+            isMounted = false;
+        };
+    }, []);
 
-    return (<h1>Groups </h1>);
+
+
+
+    return (
+        
+        <s.TableStyles>
+            <Table columns={columns} data={data} />
+        </s.TableStyles>
+            
+        
+    );
+
+
+
+
+    //return (<h1>Groups </h1>);
 }
 
 export default Groups;
