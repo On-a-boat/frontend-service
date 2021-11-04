7170b5571c59faeace7ec4599a2ea66e88a45e45
soosoo3 <sooyoun1999@gmail.com>
2021-11-04 10:19:59 +0900

    stringified selected rows

--- src/client/content/crm/CRM.js
+++ src/client/content/crm/CRM.js
@@ -4,10 +4,21 @@ import axios from "axios";
 import { DataGrid } from "@mui/x-data-grid";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import * as s from "./CRM.styles";
+
 
 export default function DataTable() {
-  const [data, setData] = useState([]);
+  const [rows, setRows] = useState([]);
   const [groupName, setGroupName] = useState("");
+  const [open, setOpen] = React.useState(false);
+  const [userId, setUserId] = useState([]);
+  const [selectedRows, setSelectedRows] = React.useState([]);
+
 
   // Table columns
   const columns = [
@@ -24,24 +35,30 @@ export default function DataTable() {
     // },
   ];
 
-  // handle adding groups
-  const handleAddGroups = () => {
-    const Ymd = (date) => date.toISOString().slice(0, 10);
-    const selectedRows = localStorage.getItem("selected");
+  // // handle adding groups
+  // const handleAddGroups = () => {
+  //   const Ymd = (date) => date.toISOString().slice(0, 10);
+  //   const selectedRows = localStorage.getItem("selected");
 
-    console.log("button pressed");
-    if (selectedRows) {
-      axios
-        .post("https://backend.weeyapp-crm-on-a-boat.com/group", {
-          groupName: "random name fix this later",
-          users: selectedRows,
-          userCount: selectedRows.length,
-          dateCreated: Ymd(new Date()),
-        })
-        .then((response) => {
-          console.log(response);
-        });
-    }
+  //   console.log("button pressed");
+  //   if (selectedRows) {
+  //     axios
+  //       .post("https://backend.weeyapp-crm-on-a-boat.com/group", {
+  //         groupName: "random name fix this later",
+  //         users: selectedRows,
+  //         userCount: selectedRows.length,
+  //         dateCreated: Ymd(new Date()),
+  //       })
+  //       .then((response) => {
+  //         console.log(response);
+  //       });
+  //   }
+  // };
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
   };
 
   // Fetch users data from the Database.
@@ -54,7 +71,7 @@ export default function DataTable() {
         );
 
         if (isMounted) {
-          setData(users.data);
+          setRows(users.data);
         }
 
         console.log(users);
@@ -68,27 +85,102 @@ export default function DataTable() {
     };
   }, []);
 
+  const makeGroup = () => {
+    const Ymd = (date) => date.toISOString().slice(0, 10);
+    const selectedRows = JSON.parse(localStorage.getItem("selected"));
+    var updateId = [];
+    console.log("button pressed");
+
+    selectedRows.forEach((row) => {
+      updateId.push(row.UserId);
+    });
+    setUserId(updateId);
+
+    if (userId.length > 0 && groupName != "") {
+      axios
+        .post("https://backend.weeyapp-crm-on-a-boat.com/group", {
+          groupName: groupName,
+          users: userId,
+          users: "" + userId,
+          userCount: userId.length,
+          dateCreated: new Date().toLocaleDateString(),
+          dateCreated: Ymd(new Date()),
+        })
+        .then((response) => {
+          console.log(response);
+        });
+    }
+  };
+
   return (
     <div style={{ height: 630, width: "80%" }}>
+      <s.CreateGroupModalButton variant="outlined" onClick={handleClickOpen}>
+        + &ensp; Create New Group
+      </s.CreateGroupModalButton>
+
+      {/* popup */}
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>CREATE A NEW GROUP</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Select users that you wish to include in your group, give it a name
+            then click CREATE.
+          </DialogContentText>
+          <TextField
+            value={groupName || ""}
+            onChange={(e) => {
+              setGroupName(e.target.value || "");
+            }}
+            autoFocus
+            margin="dense"
+            label="Enter group name"
+            fullWidth
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => makeGroup()}>Create</Button>
+        </DialogActions>
+      </Dialog>
+
       <DataGrid
-        rows={data}
+        rows={rows}
         columns={columns}
         pageSize={10}
         checkboxSelection
         rowsPerPageOptions={[5]}
-        onSelectionModelChange={(newSelection) => {
-          localStorage.setItem("selected", newSelection);
+        onSelectionModelChange={(ids) => {
+          const selectedIDs = new Set(ids);
+          const selectedRows = rows.filter((row) =>
+            selectedIDs.has(row.id)
+          );
+          console.log(selectedRows);
+          setSelectedRows(selectedRows);
+
         }}
+        {...rows}
+
+
+
       />
-      <form onSubmit={handleAddGroups}>
-        <input
-          type="text"
-          placeholder="Enter Group Name"
-          name="gname"
-          required
-        />
-        <button>Add Groups</button>
-      </form>
+      <pre style={{ fontSize: 10 }}>
+        {JSON.stringify(selectedRows, null, 4)}
+      </pre>
+
+
+
     </div>
   );
 }
+
+
+// <form onSubmit={handleAddGroups}>
+//         <input
+//           type="text"
+//           placeholder="Enter Group Name"
+//           name="gname"
+//           required
+//         />
+//         <button>Add Groups</button>
+//       </form>
+
