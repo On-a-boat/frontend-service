5a7c87eafb6819712b415d3b06a78eebd373bf7f
soosoo3 <sooyoun1999@gmail.com>
2021-11-04 20:24:27 +0900

    temp

--- src/client/content/crm/CRM.js
+++ src/client/content/crm/CRM.js
@@ -28,9 +28,9 @@ export default function DataTable() {
     { field: "lastName", headerName: "Last Name", width: 190 },
     { field: "age", headerName: "Age", width: 130 },
     { field: "gender", headerName: "Gender", width: 130 },
-    { field: "email", headerName: "Email", width: 360 },
+    { field: "email", headerName: "Email", width: 300 },
     {
-      field: "Link", headerName: "link", width: 190,
+      field: "Link", headerName: "link", width: 10,
       renderCell: (params) => (<Link href={`/userprofile/${params.value}`}>Link</Link>)
     },
     // {
@@ -40,25 +40,54 @@ export default function DataTable() {
     // },
   ];
 
-  // // handle adding groups
-  // const handleAddGroups = () => {
-  //   const Ymd = (date) => date.toISOString().slice(0, 10);
-  //   const selectedRows = localStorage.getItem("selected");
+  const makeGroup = () => {
+    const Ymd = (date) => date.toISOString().slice(0, 10);
+    const selectedRows = JSON.parse(localStorage.getItem("selectedRows"));
+    var updateId = [];
+    console.log("button pressed");
 
-  //   console.log("button pressed");
-  //   if (selectedRows) {
-  //     axios
-  //       .post("https://backend.weeyapp-crm-on-a-boat.com/group", {
-  //         groupName: "random name fix this later",
-  //         users: selectedRows,
-  //         userCount: selectedRows.length,
-  //         dateCreated: Ymd(new Date()),
-  //       })
-  //       .then((response) => {
-  //         console.log(response);
-  //       });
-  //   }
-  // };
+    selectedRows.forEach((row) => {
+      updateId.push(row.UserId);
+    });
+    setUserId(updateId);
+
+    if (userId.length > 0 && groupName != "") {
+      console.log("Work");
+
+      axios
+        .post("https://backend.weeyapp-crm-on-a-boat.com/group", {
+          groupName: groupName,
+          users: userId,
+          users: "" + userId,
+          userCount: userId.length,
+          dateCreated: new Date().toLocaleDateString(),
+          dateCreated: Ymd(new Date()),
+        })
+        .then((response) => {
+          console.log(response);
+        });
+    }
+  };
+
+  // handle adding groups
+  const handleAddGroups = () => {
+    const Ymd = (date) => date.toISOString().slice(0, 10);
+    const selectedRows = localStorage.getItem("selected");
+
+    console.log("button pressed");
+    if (selectedRows) {
+      axios
+        .post("https://backend.weeyapp-crm-on-a-boat.com/group", {
+          groupName: "random name fix this later",
+          users: selectedRows,
+          userCount: selectedRows.length,
+          dateCreated: Ymd(new Date()),
+        })
+        .then((response) => {
+          console.log(response);
+        });
+    }
+  };
   const handleClickOpen = () => {
     setOpen(true);
   };
@@ -90,37 +119,10 @@ export default function DataTable() {
     };
   }, []);
 
-  const makeGroup = () => {
-    const Ymd = (date) => date.toISOString().slice(0, 10);
-    const selectedRows = JSON.parse(localStorage.getItem("selectedRows"));
-    var updateId = [];
-    console.log("button pressed");
 
-    selectedRows.forEach((row) => {
-      updateId.push(row.UserId);
-    });
-    setUserId(updateId);
-
-    if (userId.length > 0 && groupName != "") {
-      console.log("Work");
-
-      axios
-        .post("https://backend.weeyapp-crm-on-a-boat.com/group", {
-          groupName: groupName,
-          users: userId,
-          users: "" + userId,
-          userCount: userId.length,
-          dateCreated: new Date().toLocaleDateString(),
-          dateCreated: Ymd(new Date()),
-        })
-        .then((response) => {
-          console.log(response);
-        });
-    }
-  };
 
   return (
-    <div style={{ height: "80%", width: "1190px" }}>
+    <div style={{ height: "80%", width: "1180px" }}>
       <s.CreateGroupModalButton variant="outlined" onClick={handleClickOpen}>
         + &ensp; Create New Group
       </s.CreateGroupModalButton>
@@ -146,7 +148,7 @@ export default function DataTable() {
           />
         </DialogContent>
         <DialogActions>
-          <Button onClick={() => makeGroup()}>Create</Button>
+          <Button onClick={() => handleAddGroups()}>Create</Button>
         </DialogActions>
       </Dialog>
 
@@ -163,13 +165,14 @@ export default function DataTable() {
           );
           console.log(selectedRows);
           setSelectedRows(selectedRows);
+          localStorage.setItem("selected", JSON.stringify(selectedRows));
 
         }}
         {...rows}
       />
-      {/* <pre style={{ fontSize: 10 }}>
+      <pre style={{ fontSize: 10 }}>
         {JSON.stringify(selectedRows, null, 4)}
-      </pre> */}
+      </pre>
     </div>
   );
 }
